a6e50bdddeff38507014447d6ebedfd1abc79eb2
Juli Mestieri (Projecte Ictineo SCCL) <juli@projecteictineo.com>
2014-09-21 16:02:18 +0200

    node blog

--- templates/node--article.tpl.php
+++ templates/node--article.tpl.php
@@ -6,45 +6,37 @@
  * Complete documentation for this file is available online.
  * @see https://drupal.org/node/1728164
  */
-dsm($content);
 ?>
 <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <?php if ($title_prefix || $title_suffix || $display_submitted || $unpublished || !$page && $title): ?>
-    <header>
-      <?php print render($title_prefix); ?>
-      <?php if (!$page && $title): ?>
-        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-
-      <?php if ($unpublished): ?>
-        <mark class="unpublished"><?php print t('Unpublished'); ?></mark>
-      <?php endif; ?>
-    </header>
-  <?php endif; ?>
   <?php
-    // We hide the comments and links now so that we can render them later.
     hide($content['comments']);
     hide($content['links']);
     hide($content['field_tags']);
     hide($content['print_links']);
   ?>
   <div class="col-left">
-    <?php if ($title): ?>
-      <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-    <?php endif; ?>
+    <header>
+      <?php if ($title): ?>
+        <h1<?php print $title_attributes; ?> id="page-title"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php if ($unpublished): ?>
+        <mark class="unpublished"><?php print t('Unpublished'); ?></mark>
+      <?php endif; ?>
+    </header>
     <?php print render($content); ?>
   </div>
   <div class="col-right">
     <?php if ($display_submitted): ?>
-      <p class="submitted">
+      <span class="submitted">
+        <h4><?php print (t('Author')); ?></h4>
         <?php print $user_picture; ?>
         <?php print $submitted; ?>
-      </p>
+      </span>
     <?php 
       endif;
       print render($content['field_tags']);
+      print ('<h4>' . t('Share') . '</h4>');
       print render($content['print_links']);
     ?>
   </div>
